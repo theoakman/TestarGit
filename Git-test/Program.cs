6e0c565d454f9201f6ee02fbf01620093ac7f3f2
William Ekman <william@ekman.nu>
2026-09-17 11:21:49 +0200

    Add project files.

---
+++ Git-test/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Git_test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+
+        }
+    }
+}
